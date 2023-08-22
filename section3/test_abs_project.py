ed9c97252892038bc95246196244ae75cd93e095
Topanov Dmitrii <topanovda@lad24.ru>
2023-08-22 09:28:09 +0300

    новые уроки2

--- section3/test_abs_project.py
+++ section3/test_abs_project.py
@@ -1,7 +1,37 @@
-def test_abs1():
-    assert abs(-42) == 42, "Should be absolute value of a number"
+# Пример один
+# def test_abs1():
+#     assert abs(-42) == 42, "Should be absolute value of a number"
+# if __name__ == "__main__":
+#     test_abs1()
+#     print("All tests passed!")
+
+
+# Пример второй
+# def test_abs1():
+#     assert abs(-42) == 42, "Should be absolute value of a number"
+
+
+# def test_abs2():
+#     assert abs(-42) == -42, "Should be absolute value of a number"
+
+
+# if __name__ == "__main__":
+#     test_abs1()
+#     test_abs2()
+#     print("Everything passed")
+
+
+# Пример три
+import unittest
+
+
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+
+    def test_abs2(self):
+        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
 
 
 if __name__ == "__main__":
-    test_abs1()
-    print("All tests passed!")
+    unittest.main()
