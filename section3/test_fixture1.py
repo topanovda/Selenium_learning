c04c432ccd0af62c065fab6b8fc3b200c211a830
Topanov Dmitrii <topanovda@lad24.ru>
2023-08-30 11:56:10 +0300

    edit fixture1

--- section3/test_fixture1.py
+++ section3/test_fixture1.py
@@ -1,42 +1,46 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-class TestMainPage1:
-    @classmethod
-    def setup_class(self):
-        print("\nstart browser for test suite..")
-        self.browser = webdriver.Chrome()
+# class TestMainPage1:
+#     @classmethod
+#     def setup_class(self):
+#         print("\nstart browser for test suite..")
+#         self.driver = webdriver.Chrome()
 
-    @classmethod
-    def teardown_class(self):
-        print("quit browser for test suite..")
-        self.browser.quit()
+#     @classmethod
+#     def teardown_class(self):
+#         print("quit browser for test suite..")
+#         self.driver.quit()
 
-    def test_guest_should_see_login_link(self):
-        self.browser.get(link)
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+#     def test_guest_should_see_login_link(self):
+#         self.driver.get(link)
+#         self.driver.find_element("css selector", "#login_link")
 
-    def test_guest_should_see_basket_link_on_the_main_page(self):
-        self.browser.get(link)
-        self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+#     def test_guest_should_see_basket_link_on_the_main_page(self):
+#         self.driver.get(link)
+#         self.driver.find_element("css selector", ".basket-mini .btn-group > a")
 
 
 class TestMainPage2:
     def setup_method(self):
         print("start browser for test..")
-        self.browser = webdriver.Chrome()
+        self.driver = webdriver.Chrome()
 
     def teardown_method(self):
         print("quit browser for test..")
-        self.browser.quit()
+        self.driver.quit()
 
     def test_guest_should_see_login_link(self):
-        self.browser.get(link)
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        self.driver.get(link)
+        self.driver.find_element("css selector", "#login_link")
 
     def test_guest_should_see_basket_link_on_the_main_page(self):
-        self.browser.get(link)
-        self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+        self.driver.get(link)
+        self.driver.find_element("css selector", ".basket-mini .btn-group > a")
