c29a4288bf5a90eafd1ef71b568d128dedbddb2b
Topanov Dmitrii <topanovda@lad24.ru>
2023-09-18 17:20:03 +0300

    add fixture browser default

--- section3/conftest.py
+++ section3/conftest.py
@@ -7,10 +7,36 @@ service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name",
+        action="store",
+        default="chrome",
+        help="Choose browser: chrome or firefox",
+    )
+
+
 @pytest.fixture(scope="function")
-def driver():
-    print("\nstart browser for test..")
-    driver = webdriver.Chrome()
+def driver(request):
+    browser_name = request.config.getoption("browser_name")
+    driver = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        driver = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        driver = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield driver
     print("\nquit browser..")
     driver.quit()
+
+
+# @pytest.fixture(scope="function")
+# def driver():
+#     print("\nstart browser for test..")
+#     driver = webdriver.Chrome()
+#     yield driver
+#     print("\nquit browser..")
+#     driver.quit()
---
+++ section3/test_parser.py
@@ -0,0 +1,6 @@
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link(driver):
+    driver.get(link)
+    driver.find_element("css selector", "#login_link")
