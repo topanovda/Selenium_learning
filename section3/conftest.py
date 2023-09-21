c7929dae402a411215f51d5c8197677cec2d622a
Topanov Dmitrii <topanovda@lad24.ru>
2023-09-21 17:28:11 +0300

    изменения в conftest + задания

--- section3/conftest.py
+++ section3/conftest.py
@@ -1,10 +1,6 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
 
 
 def pytest_addoption(parser):
